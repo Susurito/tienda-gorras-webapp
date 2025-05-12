5ad2576c8cd1720847a5a080fcb9dbd5a2771aea
Jorge <Susurito@users.noreply.github.com>
2025-05-12 10:20:25 -0600

    Ya?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-screen/registro-usuarios-screen.component';
+import { MujerScreenComponent } from './screens/mujer-screen/mujer-screen.component';
+import { PromocionesScreenComponent } from './screens/promociones-screen/promociones-screen.component';
+import { PromocioneshombreScreenComponent } from './screens/promocioneshombre-screen/promocioneshombre-screen.component';
+import { PromocionesmujerScreenComponent } from './screens/promocionesmujer-screen/promocionesmujer-screen.component';
+import { HombreScreenComponent } from './screens/hombre-screen/hombre-screen.component';
+import { PagoScreenComponent } from './screens/pago-screen/pago-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeScreenComponent,pathMatch: 'full'},
+  { path: 'login', component: LoginScreenComponent,pathMatch: 'full'},
+  { path: 'registro-usuarios', component: RegistroUsuariosScreenComponent,pathMatch: 'full'},
+  { path: 'hombre', component: HombreScreenComponent,pathMatch: 'full'},
+  { path: 'mujer', component: MujerScreenComponent,pathMatch: 'full'},
+  { path: 'promociones', component: PromocionesScreenComponent,pathMatch: 'full'},
+  { path: 'promociones-hombre', component: PromocioneshombreScreenComponent,pathMatch: 'full'},
+  { path: 'promociones-mujer', component: PromocionesmujerScreenComponent,pathMatch: 'full'},
+  { path: 'pago', component: PagoScreenComponent,pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
